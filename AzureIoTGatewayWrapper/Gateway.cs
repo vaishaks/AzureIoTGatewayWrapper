44e798fd7995788f2164ed5497d80cf7d2a24f51
Vaishak Salin <vaishak93@gmail.com>
2017-03-01 18:07:55 +0530

    Initial commit

---
+++ AzureIoTGatewayWrapper/Gateway.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AzureIoTGatewayWrapper
+{
+    public class Gateway
+    {
+        [DllImport(
+            "gateway.dll",
+            CharSet = CharSet.Ansi,
+            EntryPoint = "Gateway_CreateFromJson",
+            CallingConvention = CallingConvention.Cdecl
+        )]
+        public static extern IntPtr CreateFromJson(string file_path);
+
+        [DllImport(
+            "gateway.dll",
+            EntryPoint = "Gateway_Destroy",
+            CallingConvention = CallingConvention.Cdecl
+        )]
+        public static extern void Destroy(IntPtr gateway);
+    }
+}
